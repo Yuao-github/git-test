0bb957fccfdf7c8d4313a9b768f308b2de07f64b
aoyu <1790306716@qq.com>
2022-05-10 18:28:46 +0800

    cold-fix first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("master4");
         System.out.println("master5");
         System.out.println("master6");
+        System.out.println("cold-fix1");
     }
 }
