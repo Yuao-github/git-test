aded8c1c4ed14c1d114e1d64517c42dca833188f
aoyu <1790306716@qq.com>
2022-05-10 18:23:37 +0800

    sixth commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println("master3");
         System.out.println("master4");
         System.out.println("master5");
+        System.out.println("master6");
     }
 }
